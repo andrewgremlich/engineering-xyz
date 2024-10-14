d2e844f1505c5b7219f620038a575b452b208544
Andrew Gremlich <agremlich@anonyome.com>
2024-10-14 11:00:07 -0600

    programming languages and styling updates

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,27 @@ import { GravatarProfile } from "@/components/GravatarProfile";
 import { GravatarAccounts } from "@/components/GravatarAccounts";
 // import { Availability } from "@/components/Availability";
 import { Projects } from "@/components/Projects";
+import { ProgrammingLanguages } from "@/components/ProgrammingLanguages";
 
 export default async function Home() {
 	return (
 		<main>
-			<div className="w-full relative h-48 md:h-72">
+			<div className="w-full relative h-40">
 				<Image
 					src="/wasatch_pano.jpeg"
 					priority={true}
 					alt="Wasatch Mountain Range"
 					fill={true}
 					className="object-cover relative z-0 animate-fadeIn"
-					quality={100}
+					quality={70}
 				/>
-				<div className="w-full h-full relative bg-blue-300/60 dark:bg-blue-950/80 z-10" />
-				<div className="w-full h-32 absolute bottom-0 bg-gradient-to-t from-slate-900 to-transparent" />
+				<div className="w-full h-full relative bg-emerald-300/60 dark:bg-emerald-950/80 z-10" />
+				<div className="w-full h-32 absolute bottom-0 bg-gradient-to-t from-emerald-900 to-transparent" />
 			</div>
 
-			<div className="max-w-prose w-full md:m-auto relative -top-28 z-20 p-16 bg-gradient-to-t from-slate-100 dark:from-slate-900 to-transparent">
+			<div className="max-w-prose w-full md:m-auto relative -top-28 z-20 p-16 bg-gradient-to-t from-emerald-100 dark:from-emerald-900 to-transparent rounded-b-2xl">
 				<GravatarProfile />
+				<ProgrammingLanguages />
 				<GravatarAccounts />
 				{/* <Availability /> */}
 
---
+++ components/ProgrammingLanguages.tsx
@@ -0,0 +1,70 @@
+import { randomUUID } from "node:crypto";
+import {
+	SiNextdotjs,
+	SiTypescript,
+	SiReact,
+	SiRust,
+	SiJavascript,
+	SiAngular,
+	SiCss3,
+	SiNodedotjs,
+	SiFirebase,
+	SiPostgresql,
+	SiPlaywright,
+	SiExpress,
+	SiDeno,
+	SiRedux,
+	SiRemix,
+	SiNestjs,
+	SiPrisma,
+	SiAwsamplify,
+	SiAwslambda,
+	SiDocker,
+	SiTailwindcss,
+	SiAmazonwebservices,
+} from "react-icons/si";
+
+export const ProgrammingLanguages = async () => {
+	return (
+		<aside className="mb-10">
+			<div className="flex flex-wrap justify-center">
+				{[
+					{ icon: SiTypescript, label: "TypeScript" },
+					{ icon: SiJavascript, label: "JavaScript" },
+					{ icon: SiReact, label: "React" },
+					{ icon: SiRedux, label: "Redux" },
+					{ icon: SiNextdotjs, label: "Next.js" },
+					{ icon: SiRemix, label: "Remix" },
+					{ icon: SiAngular, label: "Angular" },
+					{ icon: SiCss3, label: "CSS3" },
+					{ icon: SiTailwindcss, label: "Tailwind CSS" },
+					{ icon: SiExpress, label: "Express" },
+					{ icon: SiNestjs, label: "NestJS" },
+					{ icon: SiDeno, label: "Deno" },
+					{ icon: SiNodedotjs, label: "Node.js" },
+					{ icon: SiRust, label: "Rust" },
+					{ icon: SiAwsamplify, label: "AWS Amplify" },
+					{ icon: SiAwslambda, label: "AWS Lambda" },
+					{ icon: SiFirebase, label: "Firebase" },
+					{ icon: SiAmazonwebservices, label: "Amazon Web Services" },
+					{ icon: SiPostgresql, label: "PostgreSQL" },
+					{ icon: SiPrisma, label: "Prisma" },
+					{ icon: SiPlaywright, label: "Playwright" },
+					{ icon: SiDocker, label: "Docker" },
+				].map(({ icon: Icon, label }) => (
+					<div key={randomUUID()} className="m-6">
+						<figure
+							className="flex flex-col items-center"
+							aria-labelledby={`label-${label}`}
+						>
+							<Icon size={32} className="mb-2" aria-hidden="true" />
+							<figcaption id={`label-${label}`} className="w-full text-sm">
+								{label}
+							</figcaption>
+						</figure>
+					</div>
+				))}
+			</div>
+		</aside>
+	);
+};
